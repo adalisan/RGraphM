2fd814af6c40b2520b90c6d8195fb6ef007db5a8
Sancar Adali <sadali@bbn.com>
2016-11-16 22:22:12 -0500

    Function seem to be working properly. Returns identity matrix as result

--- src/graphmatch_rcpp.cpp
+++ src/graphmatch_rcpp.cpp
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <stdexcept>
 #include <gsl/gsl_matrix.h>
+
 #include <gsl/gsl_permutation.h>
 
 #include "graphm/graph.h"
@@ -22,7 +23,7 @@ using namespace Rcpp;
 // declare a dependency on the RcppGSL package; also activates plugin
 // (but not needed when ’LinkingTo: RcppGSL’ is used with a package)
 //
-// [[Rcpp::depends(RcppGSL)]]
+// [[Rcpp::depends(RcppGSL,Rcpp)]]
 // [[Rcpp::export]]
 Rcpp::List run_graph_match(const RcppGSL::Matrix& A, const RcppGSL::Matrix& B, const Rcpp::List& algorithm_params){
   graph graphm_obj_A(A);
@@ -51,19 +52,35 @@ Rcpp::List run_graph_match(const RcppGSL::Matrix& A, const RcppGSL::Matrix& B, c
   	}
   }
   //exp.read_config(conc_params_string);
-
+  Rcpp::NumericMatrix P(A.nrow(),A.ncol());
  	try {
    exp.run_experiment(graphm_obj_A, graphm_obj_B);
- 		gsl_matrix *tmp =  exp.get_P_result(0) ;
-    RcppGSL::Matrix P(tmp); // ((gsl_matrix *) exp.get_P_result(0) )	;
- //const RcppGSL::matrix<double> P (gsl_matrix_
+
+    RcppGSL::Matrix P_tmp ( exp.get_P_result(0) )	;
+    if ( P_tmp != NULL ) {
+    try{
+	    for (int j =0 ; j< P_tmp.nrow(); j++){
+	    	for (int k =0 ; k< P_tmp.ncol(); k++){
+	      P(j,k) = P_tmp(j,k);
+	      }
+	    }
+     }
+    catch (...){
+    	Rf_error("could not copy graphm result to Rcpp matrix");
+    	return Rcpp::List();
+
+    }
+    }
+    else{
+    	Rf_error("graphm returned NULL as matrix pointer");
+    }
+    	//const RcppGSL::matrix<double> P (gsl_matrix_
 
  	 	Rcpp::List  res = Rcpp::List::create(
-//   	Rcpp::Named("debugprint_file") = algorithm_params["debugprint_file"],
- 	 	//);
- 	 		//
-    Rcpp::Named("Pmat") = P );
-  P.free();
+   	Rcpp::Named("debugprint_file") = algorithm_params["debugprint_file"],
+    Rcpp::Named("Pmat") = P
+ 	 		);
+  P_tmp.free();
  	 return res;
 	} catch( std::exception &ex) {
 		Rf_error(ex.what());
